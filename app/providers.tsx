a2262233ae42d0fa3aa222d404f8d8941c600475
Miracle Ibharokhonre <miracleosemudiahen@hotmail.com>
2024-10-13 14:30:28 +0100

    updated clerkjs

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,7 +2,6 @@
 import { ThemeProvider } from "@/components/global/theme-provider";
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "@/components/ui/toaster";
 
 function Providers({ children }: { children: React.ReactNode }) {
--- utils/actions.ts
+++ utils/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import prisma from "./db";
-import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import { JobType, CreateAndEditJobType, createAndEditJobSchema } from "./types";
 import { redirect } from "next/navigation";
 import { Prisma } from "@prisma/client";
